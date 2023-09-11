7ed43349fe56ed1a2d9cfc2dd4470be31beb17e8
masatomo57 <akagawa.code@gmail.com>
2023-09-11 12:16:05 +0900

    dbにユーザーを登録

--- src/auth/auth.ts
+++ src/auth/auth.ts
@@ -1,7 +1,10 @@
+import { app } from "@/lib/firebase"
 import { createUserWithEmailAndPassword, getAuth, sendEmailVerification } from "firebase/auth"
+import { collection, doc, getFirestore, setDoc } from "firebase/firestore"
+import { use, useId } from "react"
 
 
-export const signin = async(email:string, password:string) => {
+export const signup = async(name:string, email:string, password:string) => {
     const auth = getAuth()
     const userCredential = await createUserWithEmailAndPassword(
         auth,
@@ -13,6 +16,13 @@ export const signin = async(email:string, password:string) => {
     if (!auth.currentUser) {
         throw new Error("User not found")
     }
-    auth.currentUser
-
+    const firestore = getFirestore(app)
+    const usersRef = doc(firestore, `users/${auth.currentUser.uid}`)
+    const result = await setDoc(usersRef, {
+        id: auth.currentUser.uid,
+        name: name,
+        email: email,
+        money: 10000
+    })
+    console.log(result)
 }
